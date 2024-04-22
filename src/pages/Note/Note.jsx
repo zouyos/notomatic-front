55153d9ca4d2fa173c3d9cab8c5070f5c9134ff9
zouyos <hamza.benketaf@gmail.com>
2024-04-22 10:20:59 +0200

    Revert to v1.0

--- src/pages/Note/Note.jsx
+++ src/pages/Note/Note.jsx
@@ -1,7 +1,7 @@
 import { useNavigate, useParams, useSearchParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { NoteForm } from "components/NoteForm/NoteForm";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { deleteNote, updateNote } from "store/note/note-slice";
 import { NoteAPI } from "api/note-api";
 
@@ -21,17 +21,7 @@ export function Note() {
   const [isEditable, setIsEditable] = useState(false);
 
   async function submit(formValues) {
-<<<<<<< HEAD
     const updatedNote = await NoteAPI.update({ ...formValues, id: note.id });
-=======
-    const updatedNote = await NoteAPI.update({
-      ...formValues,
-      id: note.id,
-      userId: note.userId,
-      created_at: note.created_at,
-      modified_at: new Date().toLocaleDateString(),
-    });
->>>>>>> baa62e322f011a6a75546ce528a7eba1a63222f7
     dispatch(updateNote(updatedNote));
     setIsEditable(false);
   }
@@ -43,10 +33,6 @@ export function Note() {
       navigate("/");
     }
   }
-
-  useEffect(() => {
-    dispatch(updateNote(note));
-  }, [isEditable]);
 
   return (
     <>
